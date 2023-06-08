4c9089d914c704e9c1eda58ed81a18a44cdad528
Omer Can Bozkurt <omerbozkurt912@gmail.com>
2023-06-09 00:59:12 +0300

    database connection settings reconfigured

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"; // eslint-disable-line
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuaOgtUq1CKjYX6CXmVj6ypbHu8F1bKEI",
-  authDomain: "quasarfinal.firebaseapp.com",
-  projectId: "quasarfinal",
-  storageBucket: "quasarfinal.appspot.com",
-  messagingSenderId: "1048674802406",
-  appId: "1:1048674802406:web:60eb93a11e556807d8b927",
-  measurementId: "G-JG5HTYZRJH",
+  apiKey: "AIzaSyBB7JDzI0S-LdF8a_3F9a5uZzblSL2vsz8",
+  authDomain: "quasargithub.firebaseapp.com",
+  projectId: "quasargithub",
+  storageBucket: "quasargithub.appspot.com",
+  messagingSenderId: "644417494894",
+  appId: "1:644417494894:web:5887a69d8225132c9eeb7f",
+  measurementId: "G-R8894LB1SW",
 };
 
 const fapp = initializeApp(firebaseConfig);
